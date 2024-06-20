bad1b6c6c7b24eef8d30968bada93577d365edaa
WorkingRaven2817 <164280595+WorkingRaven2817@users.noreply.github.com>
2024-06-19 19:29:44 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 function startClassification()
 {
   navigator.mediaDevices.getUserMedia({ audio: true});
-  classifier = ml5.soundClassifier(https://teachablemachine.withgoogle.com/models/CKfpud5wt/)
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/CKfpud5wt/model.json',modelReady);
 }
   function modelReady(){
-    classifier.classify( gotResults);
+    classifier.classify(gotResults);
 }
 
 function gotResults(error,result){
     if (error) {
       console.error(error);
-      { else }
+      } else {
       console.log(results);
       random_number_r = Math.floor(Math.random() *255) + 1;
       random_number_g = Math.floor(Math.random() *255) + 1;
